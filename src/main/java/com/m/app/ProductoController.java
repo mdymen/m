88ef586fa0cf2d629f9d10773f3b6f778d6337c7
mdymen <msn@dymenstein.com>
2015-06-04 20:34:15 -0300

    cambios

--- src/main/java/com/m/app/ProductoController.java
+++ src/main/java/com/m/app/ProductoController.java
@@ -1,25 +1,39 @@
 package com.m.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.m.fachade.FProducto;
+import com.m.fachade.FVenta;
+import com.m.object.Producto;
+import com.m.object.Venta;
+
 @Controller
 public class ProductoController {
 
+	@Autowired
+	FProducto producto;
+	
+	@Autowired
+	FVenta venta;
+	
 	@RequestMapping(value="/productos", method = RequestMethod.GET)
 	public String productos() {
 		return "productos";
 	}
 	
 	@RequestMapping(value="/venta", method = RequestMethod.POST)
-	public String vender(Model model) {
+	public String vender(Venta venta) {
+		this.venta.vender(venta);
 		return "productos";
 	}
 	
 	@RequestMapping(value="/quitar", method = RequestMethod.POST)
-	public String quitar(Model model) {
+	public String quitar(Venta venta) {
+		this.quitar(venta);
 		return "productos";
 	}
 	
--- src/main/java/com/m/fachade/FVenta.java
+++ src/main/java/com/m/fachade/FVenta.java
@@ -1,5 +1,29 @@
 package com.m.fachade;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.m.object.Venta;
+
 public class FVenta {
+
+	@PersistenceContext
+	static EntityManager manager;
+	
+	public void vender(Venta venta) {
+	    manager = Manager.getManager().getInstance();
+		
+		manager.getTransaction().begin();
+	    
+	    manager.persist(venta);
+	    
+	    manager.getTransaction().commit();
+		
+	}
+
+	public void quitar(Venta venta) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
